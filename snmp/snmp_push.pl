1bf8c0c5777f226e186f458dd5b8e33b88aa29b0
House <118424356+house40105@users.noreply.github.com>
2023-06-21 11:35:49 +0800

    Add files via upload

--- snmp/snmp_push.pl
+++ snmp/snmp_push.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl
 
-my $cpu_usage = sprintf("%.2f",`snmpwalk -v2c -c private 127.0.0.1 laLoad.1 | awk -F' ' '{print \$4}' | tr -d '%'`);
+my $cpu_usage = sprintf("%.2f",`snmpwalk -v2c -c acom_snmp localhost laLoad.1 | awk -F' ' '{print \$4}' | tr -d '%'`);
 
-my $mem_usage = `snmpwalk -v2c -c private 127.0.0.1 memAvailReal | awk -F' ' '{print \$4}'`;
-my $total_mem = `snmpwalk -v2c -c private 127.0.0.1 memTotalReal | awk -F' ' '{print \$4}'`;
+my $mem_usage = `snmpwalk -v2c -c acom_snmp localhost memAvailReal | awk -F' ' '{print \$4}'`;
+my $total_mem = `snmpwalk -v2c -c acom_snmp localhost memTotalReal | awk -F' ' '{print \$4}'`;
 my $mem_usage_percentage = sprintf("%.2f", (($total_mem - $mem_usage) / $total_mem)*100);
 
 my $response = '<html><body>['.$cpu_usage.']['.$mem_usage_percentage.']</html></body>';
--- snmp/snmp_push.sh
+++ snmp/snmp_push.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 # 使用 SNMP 命令從 Linux 主機收集 CPU 使用率
-cpu_usage=$(snmpwalk -v2c -c private 127.0.0.1 ssCpuUser | awk -F' ' '{print $4}')
+cpu_usage=$(snmpwalk -v2c -c acom_snmp localhost laLoad.1 | awk -F' ' '{print $4}')
 
 # 使用 SNMP 命令從 Linux 主機收集記憶體使用率
-mem_usage=$(snmpwalk -v2c -c private 127.0.0.1 memAvailReal| awk -F' ' '{print $4}')
-total_mem=$(snmpwalk -v2c -c private 127.0.0.1 memTotalReal| awk -F' ' '{print $4}')
-mem_usage_percentage=$(((($total_mem-$mem_usage)/$total_mem)*100))
+mem_usage=$(snmpwalk -v2c -c acom_snmp localhost memAvailReal| awk -F' ' '{print $4}')
+total_mem=$(snmpwalk -v2c -c acom_snmp localhost memTotalReal| awk -F' ' '{print $4}')
+
 
 echo "$cpu_usage"
+echo "$mem_usage"
+echo "$total_mem"
+
 #echo "$mem_usage_percentage"
 # 使用 cURL 將資訊推送至 PRTG
 #curl -X POST -H "Content-Type: application/json" -d '{"cpu_usage": '$cpu_usage', "mem_usage": '$mem_usage'}' https://your-prtg-server/api/sensors/12345/data
